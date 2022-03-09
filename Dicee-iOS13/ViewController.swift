5f5e04591116fb2cf7183c9a3d871fb2a0039b00
Rohit Vishwakarma <rohitvishwakarma1819@gmail.com>
2022-03-09 13:17:14 +0530

    auto-layout-understood

--- Dicee-iOS13/ViewController.swift
+++ Dicee-iOS13/ViewController.swift
@@ -10,11 +10,26 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var diceImageView1: UIImageView!
+    @IBOutlet weak var diceImageView2: UIImageView!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        diceImageView1.image = UIImage(named: "DiceFive")
+        diceImageView2.image = UIImage(named: "DiceFour")
     }
 
-
+    @IBAction func rollButtonPressed(_ sender: UIButton) {
+        diceImageView1.image = UIImage(named: getRandomDiceImageName())
+        diceImageView2.image = UIImage(named: getRandomDiceImageName())
+    }
+    
+    func getRandomDiceImageName()->String{
+            let randomName = ["One","Two","Three","Four","Five","Six"].randomElement()
+            return "Dice"+randomName!;
+        }
+    
 }
 
